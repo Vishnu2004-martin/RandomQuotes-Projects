09c2b9809665dae6f302aa2e8731603644b2a189
Vishnu2004-martin <128040550+Vishnu2004-martin@users.noreply.github.com>
2023-04-22 11:30:35 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+<p><a  id="name" href="https://www.linkedin.com/in/vishnu-vishnu-b0b375273/">-  Vishnu</a></p>
+    </div>
+  )
+}
+
+export default Footer;
